dbf164cc0c08b28478392255b7a3740f9c98d201
Jalil <abdullacelil00@gmail.com>
2024-10-27 13:58:55 +0400

    HTML & CSS

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,13 @@
+<div class="fixed z-50 inset-0">
+    {{-- Gray background --}}
+    <div class="fixed inset-0 bg-gray-300/40"></div>
+    {{-- Modal body --}}
+    <div class="bg-white rounded m-auto fixed inset-0 max-w-2xl max-h-96">
+        <div>
+            Header
+        </div>
+        <div>
+            Body
+        </div>
+    </div>
+</div>
